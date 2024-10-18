d820ba2aa28f33d7399f10099fc0d5dfa93cabd0
renan.paula <renan.paula@tce.am.gov.br>
2024-10-18 12:18:16 -0400

    modificação: exibir erros email e senha

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 function validateFields() {
-    const emailValid = isEmailValid();
-    document.getElementById('recover-password-button').disabled = !emailValid;
-
-    const passwordValid = isPasswordValid();
-    document.getElementById('login-button').disabled = !emailValid || !passwordValid;
+    toggleButtonsDisable();
+    toggleEmailErrors();
+    togglePasswordError();
 }
 
 function isEmailValid() {
@@ -12,6 +10,38 @@ function isEmailValid() {
         return false;
     }
     return validateEmail(email);
+}
+
+function toggleEmailErrors () {
+    const email = document.getElementById('email').value;
+    if (!email) {
+        document.getElementById('email-required-error').style.display = "block";
+    } else {
+        document.getElementById('email-required-error').style.display = "none";
+    }
+
+    if (validateEmail(email)) {
+        document.getElementById('email-invalid-error').style.display = "none";
+    } else {
+        document.getElementById('email-invalid-error').style.display = "block";
+    }
+}
+
+function togglePasswordError() {
+    const password = document.getElementById('password').value;
+    if (!password) {
+        document.getElementById('password-required-error').style.display = "block";
+    } else {
+        document.getElementById('password-required-error').style.display = "none";
+    }
+}
+
+function toggleButtonsDisable() {
+    const emailValid = isEmailValid();
+    document.getElementById('recover-password-button').disabled = !emailValid;
+
+    const passwordValid = isPasswordValid();
+    document.getElementById('login-button').disabled = !emailValid || !passwordValid;
 }
 
 function isPasswordValid() {
